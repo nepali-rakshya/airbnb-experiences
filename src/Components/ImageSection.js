86db5973410531ba0967d6357618d2937fb4bad7
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-02 21:21:37 +0545

    commit

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,32 +1,35 @@
 import ImageSectionCSS from "./ImageSection.module.css";
 import cn from "classnames";
-import chair from "../img/chair.jpg";
 import guitar from "../img/guitar.jpg";
 import cook from "../img/cook.jpg";
 import cooking from "../img/cooking.jpg";
 import food from "../img/food.jpg";
 import swimming from "../img/swimming.jpg";
+import singing from "../img/singing.jpg";
+import piano from "../img/piano.jpg";
+import temple from "../img/temple.jpg";
+import home from "../img/home.jpg";
 
 const ImageSection = () => {
   return (
     <section className={cn(ImageSectionCSS.img__flexbox)}>
       <div className={ImageSectionCSS.img__first}>
-        <img src={chair} alt="" className={ImageSectionCSS.img} />
+        <img src={food} alt="" className={ImageSectionCSS.img} />
       </div>
       <div className={ImageSectionCSS.img__second}>
-        <img src={guitar} alt="" className={ImageSectionCSS.img} />
+        <img src={home} alt="" className={ImageSectionCSS.img} />
         <br />
         <img src={cook} alt="" className={ImageSectionCSS.img} />
       </div>
       <div className={ImageSectionCSS.img__third}>
-        <img src={chair} alt="" className={ImageSectionCSS.img} />
+        <img src={temple} alt="" className={ImageSectionCSS.img} />
         <br />
-        <img src={chair} alt="" className={ImageSectionCSS.img} />
+        <img src={piano} alt="" className={ImageSectionCSS.img} />
       </div>
       <div className={ImageSectionCSS.img__fourth}>
-        <img src={food} alt="" className={ImageSectionCSS.img} />
+        <img src={guitar} alt="" className={ImageSectionCSS.img} />
         <br />
-        <img src={chair} alt="" className={ImageSectionCSS.img} />
+        <img src={singing} alt="" className={ImageSectionCSS.img} />
       </div>
       <div className={ImageSectionCSS.img__fifth}>
         <img src={swimming} alt="" className={ImageSectionCSS.img} />

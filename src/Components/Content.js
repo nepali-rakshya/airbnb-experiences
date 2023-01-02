114ec47d38916294cbbc77b162561252ecadc28a
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-02 21:47:10 +0545

    commit

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,7 +3,7 @@ import ContentCSS from "./Content.module.css";
 const Content = () => {
   return (
     <section>
-      <h1 className={ContentCSS.content__title}>Online Experiences</h1>
+      <p className={ContentCSS.content__title}>Online Experiences</p>
       <p className={ContentCSS.content__para}>
         Join unique interactive activities led by one-of-a-kind hosts—all
         without leaving home.

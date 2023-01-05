56c571a8d94e3122f987feedca2a073ee6943cba
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-05 13:27:04 +0545

    commit

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -3,30 +3,28 @@ import cn from "classnames";
 
 const Cards = (props) => {
   return (
-    <section>
-      {/* Image section with the tag  */}
+    <main className={CardsCSS.main__section}>
+      <section>
+        {/* Image section with the tag  */}
 
-      <div className={CardsCSS.img__cards}>
-        <img src={props.img1} alt="writer" className={CardsCSS.img__width} />
-        <p className={CardsCSS.img__position}>{props.sale}</p>
-      </div>
+        <div className={CardsCSS.img__cards}>
+          <img src={props.img1} alt="writer" className={CardsCSS.img__width} />
+          <p className={CardsCSS.img__position}>{props.sale}</p>
+        </div>
 
-      {/* Image section with the rating and desc */}
+        {/* Image section with the rating and desc */}
 
-      <div className={cn(CardsCSS.img__details)}>
-        {/* ***** rating **** */}
-        <div className={CardsCSS["img__details--flex"]}>
+        <div className={cn(CardsCSS.img__details)}>
           <img src={props.img2} alt="rating-star" />
           <span>{props.rate}</span>
           <span className={CardsCSS.rating}>(6) • {props.country} </span>
+          <p>{props.lifeLesson}</p>
+          <p>
+            <strong> From {props.price}</strong> / person
+          </p>
         </div>
-        {/* ***** desc ***** */}
-        <p>{props.lifeLesson}</p>
-        <p>
-          <strong> From {props.price}</strong> / person
-        </p>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 };
 

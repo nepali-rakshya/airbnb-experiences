afcf75bd8f2073aae01c68bb2c11710c678f373d
nepali-rakshya <rakshyanepali440@gmail.com>
2023-01-05 22:35:25 +0545

    commit

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,41 +1,35 @@
-import writer from "../img/writer.jpg";
-import star from "../img/star.png";
-import girl1 from "../img/girl1.jpg";
-import girl2 from "../img/girl2.jpg";
-import girl3 from "../img/girl3.jpg";
-
 export default [
   {
-    img1: { writer },
+    img1: "/image/writer.jpg",
     sale: "Sold out",
-    img2: { star },
+    img2: "/image/star.png",
     rate: `${5}`,
     country: "USA",
     lifeLesson: "Life lessons with Katie",
     price: `$ ${136}`,
   },
   {
-    img1: { girl1 },
+    img1: "/image/girl1.jpg",
     sale: "Order",
-    img2: { star },
+    img2: "/image/star.png",
     rate: `${4.9}`,
     country: "England",
     lifeLesson: "Learn Digital Marketing",
     price: `$ ${143}`,
   },
   {
-    img1: { girl2 },
+    img1: "/image/girl2.jpg",
     sale: "Order",
-    img2: { star },
+    img2: "/image/star.png",
     rate: `${4.9}`,
     country: "Britain",
     lifeLesson: "Learn abroad in Sidney",
     price: `$ ${70}`,
   },
   {
-    img1: { girl3 },
+    img1: "/image/girl3.jpg",
     sale: "Sold out",
-    img2: { star },
+    img2: "/image/star.png",
     rate: `${3.9}`,
     country: "Asia",
     lifeLesson: "Let's learn coding",
